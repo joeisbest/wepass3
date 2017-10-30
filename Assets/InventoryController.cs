819970ca17537634970ab9622a8f266ab8ca2c73
Joe <s1041672@gm.pu.edu.tw>
2017-10-30 23:25:29 +0800

    InventoryTest

---
+++ Assets/InventoryController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryController : MonoBehaviour {
+
+	// Use this for initialization
+	public Transform selectedItem, selectedSlot, originalSlot;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(0)&& selectedItem!=null){
+			selectedItem.GetComponent<Collider> ().enabled = false;
+
+		}
+		if(Input.GetMouseButton(0)&& selectedItem!=null){
+			selectedItem.position = Input.mousePosition;
+			
+		}
+		else if(Input.GetMouseButtonUp(0)&& selectedItem!=null){
+			selectedItem.parent = selectedSlot;
+			selectedItem.localPosition = Vector3.zero;
+			selectedItem.GetComponent<Collider> ().enabled = true;
+
+		}
+		
+	}
+}
