81e064d3387950b513eadabe1eaa85102bd08304
Joe <s1041672@gm.pu.edu.tw>
2017-11-08 23:54:22 +0800

    失敗的道具欄位和做一半的小遊戲

---
+++ Assets/scripts/Card.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour {
+	public CardState cardState;
+	public CardPattern cardPattern;
+	public GameManager gameManager;
+	void Start () {
+		cardState = CardState.未翻牌;
+	}
+	private void OnMouseUp(){
+		if (cardState.Equals (CardState.已翻牌)) {
+			return;
+		}
+		if(gameManager.ReadyToCompareCards){
+			return;
+		}
+
+		OpenCard ();
+		gameManager.AddCardInCardComparision (this);
+		gameManager.CompareCardsInList ();
+	}
+	void OpenCard(){
+		transform.eulerAngles = new Vector3 (0, 180, 0);
+		cardState = CardState.已翻牌;
+	}
+}
+
+public enum CardState
+{
+	未翻牌,已翻牌,配對成功
+}
+public enum CardPattern
+{
+	無/*預設*/,白花,木桶,玩偶,橘偶,梯子,旗子,繩子,花
+}
---
+++ Assets/scripts/GameManager.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	public List<Card> cardComparision; 
+	void Start () {
+		
+	}
+	void AddNewCard(CardPattern cardPattern){
+		GameObject card = Instantiate (Resources.Load<GameObject> ("Prefabs/牌"));
+		card.GetComponent<Card> ().cardPattern = cardPattern;
+		card.name = "牌_" + cardPattern.ToString ();
+	}
+	public void AddCardInCardComparision(Card card){
+		cardComparision.Add (card); 
+	}
+	public bool ReadyToCompareCards{
+		get{
+				if (cardComparision.Count == 2) {		
+					return true;				
+				} 
+				else {
+					return false;
+				}
+			
+			}
+
+	}
+	public void CompareCardsInList(){
+	
+		if (ReadyToCompareCards) 
+		{
+			//Debug.Log ("Can compare cards");
+			if (cardComparision [0].cardPattern == cardComparision [1].cardPattern) 
+			{
+				Debug.Log ("Succese");
+				foreach (var card in cardComparision) {
+					card.cardState = CardState.配對成功;
+				}
+				ClearCardComparision();
+			} 
+			else 
+			{
+				Debug.Log("Fail");
+				StartCoroutine (MissMatchCards ());
+				//TurnBackCards ();
+				//ClearCardComparision();
+			}
+							
+		}
+	}
+	void ClearCardComparision(){
+		cardComparision.Clear();	
+	}
+	void TurnBackCards(){
+		foreach (var card in cardComparision) {
+			card.gameObject.transform.eulerAngles = Vector3.zero;
+			card.cardState = CardState.未翻牌;
+		}
+	}
+
+	IEnumerator MissMatchCards(){
+		yield return new WaitForSeconds (1.5f);
+		TurnBackCards ();
+		ClearCardComparision();
+	}
+}
